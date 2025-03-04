a1b52b3998dac371668cc1a5b4b4217767e2310b
Raja kumar prasad <ajarubab@gmail.com>
2025-03-04 10:46:38 +0530

    passing data from script block to template block within the same component

--- src/components/MyHome.vue
+++ src/components/MyHome.vue
@@ -2,10 +2,26 @@
     <h1>MyHome Component</h1>
     <h1>20 + 15 = {{ 20+15 }}</h1>
     <h1>The word "asfjhaskhgbalejgklgh" has {{ "asfjhaskhgbalejgklgh".length }} Characters.</h1>
+    <h1>{{ naam }} is of {{ Age }} years old and lives in {{ getCountry() }}</h1>
+    <h1>{{ naam1 = "Ramu" }} is of {{ Age1 = 34 }} years old and lives in {{ getState("Nepal") }}</h1>
 </template>
 <script>
 export default {
-    name: 'MyHome'
+    name: 'MyHome',
+    data(){
+        return{
+            naam : "Raja kumar",
+            naam1 : "",
+            Age : 28,
+            Age1 : 28,
+            getCountry : function(){
+                return "India";
+            },
+            getState : function(a){
+            return a;
+            },
+        }
+    }
 }
 </script>
 <style scoped>
