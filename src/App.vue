f1a876dee43e896282f79d6dbdb73e27e05e06c8
Raja kumar prasad <ajarubab@gmail.com>
2025-03-04 10:19:08 +0530

    creation of a new component and rendering it through app component by importing it there

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld />
+  <MyHome/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import MyHome from './components/MyHome.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    MyHome
   }
 }
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/MyHome.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>MyHome Component</h1>
+</template>
+<script>
+export default {
+    name: 'MyHome'
+}
+</script>
+<style scoped>
+h1{
+    color:blue;
+}
+</style>
