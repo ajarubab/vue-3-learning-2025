69ddd24bc19871409511d65b788ed9e893e75880
Raja kumar prasad <ajarubab@gmail.com>
2025-03-04 13:06:09 +0530

    getting the entered input values of input fields using v-mode twoway binding

--- src/components/MyHome.vue
+++ src/components/MyHome.vue
@@ -1,15 +1,23 @@
 <template>
     <h1> My Home Component</h1>
-    <h2>The Count is  {{ count }}</h2>
-    <input type="text" v-model="count">
+    <input type="text"  placeholder="Enter username" v-model="username"><br><br>
+    <input type="password" placeholder="Enter Password" v-model="password"><br><br>
+    <button v-on:click="userDetails()">Submit</button>
+    
 </template>
 <script>
     export default {
         name : 'MyHome',
         data(){
             return{
-                count : 10276,
+                username : null,
+                password : null,
             }
-        }
+        },
+        methods:{
+            userDetails(){
+                console.log("Username : ",this.username,"\nPassword : ",this.password);
+            }
+        },
     }
 </script>
