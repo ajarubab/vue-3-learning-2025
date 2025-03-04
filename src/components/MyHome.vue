13ba95ffae649a4afb23c3ee8cd9edb1f6b2ba84
Raja kumar prasad <ajarubab@gmail.com>
2025-03-04 14:25:51 +0530

    Use of for loop , while loop and list

--- src/components/MyHome.vue
+++ src/components/MyHome.vue
@@ -1,7 +1,18 @@
 <template>
-    <h1 v-if="Show">It's Day Now</h1>
-    <h1 v-else>It's Night time</h1>
-    <button v-on:click="Show=!Show">Change Time</button>
+    <h1> USE OF FOR LOOP AND WHILE LOOP</h1>
+    <ul>
+        <li v-for="x in color" :key="x">
+            {{ x }}
+        </li>
+        <br>
+        <li v-for="y in firstTable" :key="y">
+            {{ y }}
+        </li>
+        <br>
+        <li v-for="z in secondTable" :key="z">
+            {{ z }}
+        </li>
+    </ul>
     
 </template>
 <script>
@@ -9,7 +20,28 @@
         name : 'MyHome',
         data(){
             return{
-                Show : true,
+                color : ['Red','Black','Blue','Green'],
+                firstTable : this.getTableOf(5),
+                secondTable : this.getAnotherTable(12),
+            }
+        },
+        methods:{
+            getTableOf(n){
+                let res = [];
+                for(let m = 1; m<= 10; m++){
+                    res.push(n*m);
+                }
+                return res;
+                
+            },
+            getAnotherTable(n){
+                let res = [];
+                let m = 1;
+                while(m<11){
+                    res.push(n*m);
+                    m++;
+                }
+                return res;
             }
         }
     }
