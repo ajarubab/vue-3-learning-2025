7e104c525b18c150d60661a834abdb348a0761fc
Raja kumar prasad <ajarubab@gmail.com>
2025-03-04 11:13:20 +0530

    Click event of a button handled by alert in method section

--- src/components/MyHome.vue
+++ src/components/MyHome.vue
@@ -1,40 +1,13 @@
 <template>
     <h1>My Home Component</h1>
-    <h1>{{ Name }}</h1>
-    <h1>{{ getName() }}</h1>
-    <h1>{{ getCountry("India") }}</h1>
-    <h2>Other Data : {{ getData() }}</h2>
-    <h2>Other Data State: {{ getData().State }}</h2>
-    <h2>State: {{ getState() }}</h2>
-    <h2>Age: {{ getAge() }}</h2>
+    <button v-on:click="Click()">Click me</button>
 </template>
 <script>
 export default{
     name : 'MyHome',
-    data(){
-        return{
-            Name : 'Rajat Kumar',
-            Age : 28,
-        }
-    },
-    methods : {
-        getName(){
-            return "Radhe Radhe";
-        },
-        getCountry(c){
-            return c;
-        },
-        getData(){
-            return{
-                State : "Delhi",
-                Email : "Raja@kumar.in"
-            }
-        },
-        getState(){
-            return this.getData().Email;
-        },
-        getAge(){
-            return this.Age;
+    methods :{
+        Click(){
+            alert ("Button Clicked");
         }
     }
 }
