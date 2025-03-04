9abe36114a2de990fe9d343718fda4a33f8cf17e
Raja kumar prasad <ajarubab@gmail.com>
2025-03-04 15:01:41 +0530

    passing various types of data/functions from Parent component to child component

---
+++ src/components/Child.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Child components</h1>
+    <h2>Message from Home : {{ msg }}</h2>
+    <h2>Name of user from Home : {{ userDetails.naam }}</h2>
+    <h2>Age of user from Home : {{ userDetails.Age }}</h2>
+    <button v-on:click="funCall()">Click me</button>
+</template>
+<script>
+    export default{
+        name : "MyChild",
+        props:{
+            msg : String,
+            userDetails : Object,
+            funCall : Function,
+        }
+    }
+</script>
--- src/components/MyHome.vue
+++ src/components/MyHome.vue
@@ -1,24 +1,29 @@
 <template>
     <h1> My Home</h1>
-    <ol>
-        <li v-for="x in user" :key="x.name">
-            Name : {{ x.name }}, Age : {{ x.age }}
-        </li>
-    </ol>
+    <Child msg="Hello my Child" :userDetails="user" :funCall="callFunction"/>
 </template>
 <script>
-    export default {
-        name : 'MyHome',
-        data(){
-            return{
-                user : [
-                    {name: 'raja', age : 23},
-                    {name: 'raju', age : 21},
-                    {name: 'rajan', age : 28},
-                    {name: 'rajat', age : 22},
-                    {name: 'rajesh', age : 52},
-                ],
+import Child from './Child.vue';
+
+export default {
+    name: 'MyHome',
+    components: {
+        Child,
+    },
+    data() {
+        return{
+            user : {
+                naam : 'Raja',
+                Age : 28
             }
         }
+    },
+    methods:{
+        callFunction(){
+            console.log("parent function called");
+            alert("Parent function called");
+        }
     }
+}
+
 </script>
