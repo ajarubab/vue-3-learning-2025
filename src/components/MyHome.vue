6ddb6017128a08692c4a113630770589c9588df5
Raja kumar prasad <ajarubab@gmail.com>
2025-03-04 13:54:01 +0530

    implementation of if-else condition using button

--- src/components/MyHome.vue
+++ src/components/MyHome.vue
@@ -1,17 +1,15 @@
 <template>
-    <h1>Gender</h1>
-    <label for="male">Male</label>
-    <input type="radio" id="male" value="Male" name="myGender" v-model="gender"> <br><br>
-    <label for="female">Female</label>
-    <input type="radio" id="female" value="Female" name="myGender" v-model="gender"> <br><br>
-    <h2>I am a {{ gender }}.</h2>    
+    <h1 v-if="Show">It's Day Now</h1>
+    <h1 v-else>It's Night time</h1>
+    <button v-on:click="Show=!Show">Change Time</button>
+    
 </template>
 <script>
     export default {
         name : 'MyHome',
         data(){
             return{
-                gender : ""
+                Show : true,
             }
         }
     }
